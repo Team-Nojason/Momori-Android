9274b6861c4ae0072036cfa4a98a4dc00ed11efb
seak230 <113758643+seak230@users.noreply.github.com>
2023-10-03 19:03:19 +0900

    refactoring: package

--- app/src/main/java/com/nohjason/momori/ui/onboard/OnBoardScreen.kt
+++ app/src/main/java/com/nohjason/momori/ui/onboard/OnBoardScreen.kt
@@ -1,42 +1,30 @@
-package com.nohjason.momori
+package com.nohjason.momori.ui.onboard
 
-import android.print.PrintAttributes.Margins
-import androidx.compose.animation.expandHorizontally
-import androidx.compose.foundation.BorderStroke
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,12 +33,20 @@ import com.nohjason.momori.R
 @Composable
 fun OnBoardScreen(msg: String) {
 
-    val imageModifier = Modifier
-        .size(50.dp)
-        .border(BorderStroke(1.dp, Color.Black))
-        .background(Color.Yellow)
+//    val imageModifier = Modifier
+//        .size(50.dp)
+//        .border(BorderStroke(1.dp, Color.Black))
+//        .background(Color.Yellow)
 
-    Column (Modifier.padding(horizontal = 20.dp)){
+    val context = LocalContext.current
+
+    Column (
+        Modifier
+            .background(color = Color(0xffF1F1F1))
+            .padding(horizontal = 20.dp)){
+
+        Text(text = "momori", modifier = Modifier.align(CenterHorizontally))
+
         Spacer(modifier = Modifier.height(200.dp))
         Column {
             Text(text = "당신의", fontSize = 28.sp, fontWeight = FontWeight.ExtraBold)
@@ -67,22 +63,33 @@ fun OnBoardScreen(msg: String) {
         Spacer(modifier = Modifier
             .height(160.dp)
         )
-        Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()) {
+        Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()) {
             Image(
                 painter = painterResource(id = R.drawable.google),
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .size(30.dp)
-                    .border(1.dp, Color.Black, CircleShape)
+                    .size(45.dp)
+                    .clickable {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"))
+                        context.startActivity(intent)
+                    }
             )
+            Spacer(modifier = Modifier.width(34.dp))
             Image(
-                painter = painterResource(id = R.drawable.google),
+                painter = painterResource(id = R.drawable.kaka),
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
-                    .size(30.dp)
-                    .border(1.dp, Color.Black, CircleShape)
+                    .size(45.dp)
+            )
+            Spacer(modifier = Modifier.width(34.dp))
+            Image(
+                painter = painterResource(id = R.drawable.naver),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(45.dp)
             )
         }
     }
