90119fb7e1587f17f283be49079b2f1267df605b
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-10-05 18:37:22 +0900

    ui

--- app/src/main/java/com/nohjason/momori/component/appbar/MomoriTopBar.kt
+++ app/src/main/java/com/nohjason/momori/component/appbar/MomoriTopBar.kt
@@ -1,15 +1,58 @@
 package com.nohjason.momori.component.appbar
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.nohjason.momori.R
+import com.nohjason.momori.component.button.MomoriIconButton
+import com.nohjason.momori.component.theme.MomoriColor
+import com.nohjason.momori.component.theme.Title
 
 @Composable
 fun MomoriTopBar(
     modifier: Modifier = Modifier,
-    content: @Composable () -> Unit
+    titleText: String,
+    enablePrimaryButton: Boolean = false,
+    primaryButtonCallback: () -> Unit = {},
+    @DrawableRes primaryIconId: Int = R.drawable.ic_back,
+    color: Color = MomoriColor.White,
+    content: @Composable ColumnScope.() -> Unit,
 ) {
-    Column {
-        
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Spacer(modifier = Modifier.width(8.dp))
+            if (enablePrimaryButton)
+                MomoriIconButton(
+                    modifier = Modifier,
+                    iconId = primaryIconId,
+                    contentDescription = "to back",
+                    onClick = primaryButtonCallback
+                )
+            Spacer(modifier = Modifier.width(8.dp))
+            Title(text = titleText)
+            Spacer(modifier = Modifier.weight(1f))
+        }
+        content()
     }
 }
---
+++ app/src/main/java/com/nohjason/momori/ui/setting/SettingScreen.kt
@@ -0,0 +1,18 @@
+package com.nohjason.momori.ui.setting
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.nohjason.momori.component.appbar.MomoriTopBar
+import com.nohjason.momori.component.theme.Body
+
+@Composable
+fun SettingScreen() {
+    MomoriTopBar(
+        titleText = "설정"
+    ) {
+        Body(text = "그래그래 이것이 설정이다")
+    }
+}
+
+@Composable
+@Preview()
