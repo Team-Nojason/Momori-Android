4e8349d0c69711098d21729cc4fd7cce9ec37d29
seak230 <113758643+seak230@users.noreply.github.com>
2023-10-05 14:59:10 +0900

    location permission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation ("androidx.compose.ui:ui-graphics")
     implementation ("androidx.compose.ui:ui-tooling-preview")
     implementation ("androidx.compose.material3:material3")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
     androidTestImplementation (platform("androidx.compose:compose-bom:2022.10.00"))
     androidTestImplementation ("androidx.compose.ui:ui-test-junit4")
     debugImplementation ("androidx.compose.ui:ui-tooling")
--- app/src/main/java/com/nohjason/momori/ui/main/MainScreen.kt
+++ app/src/main/java/com/nohjason/momori/ui/main/MainScreen.kt
@@ -1,19 +1,28 @@
 package com.nohjason.momori.ui.main
 
+import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.util.Log
 import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.rememberCameraPositionState
+import com.nohjason.momori.util.PermissionUtil.checkAndRequestPermissions
 
 @Composable
 fun MainScreen() {
@@ -21,13 +30,49 @@ fun MainScreen() {
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(singapore, 16f)
     }
+    val context = LocalContext.current
+
+
+    /** 요청할 권한 **/
+    val permissions = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
+    var a by remember { mutableStateOf(false) }
+
+    val launcherMultiplePermissions = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissionsMap ->
+        val areGranted = permissionsMap.values.reduce { acc, next -> acc && next }
+        /** 권한 요청시 동의 했을 경우 **/
+        if (areGranted) {
+            a = false
+            Log.d("test5", "권한이 동의되었습니다.")
+        }
+        /** 권한 요청시 거부 했을 경우 **/
+        else {
+            a = true
+            Log.d("test5", "권한이 거부되었습니다.")
+        }
+    }
+
+
+    if (a) {
+        checkAndRequestPermissions(context, permissions, launcherMultiplePermissions)
+    }
+
+    LaunchedEffect(true) {
+        checkAndRequestPermissions(context, permissions, launcherMultiplePermissions)
+    }
+
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState
     ) {
         Marker(
             position = singapore,
-            title = "Sex",
+            title = "wow",
             snippet = "Marker in Singapore"
         )
     }
