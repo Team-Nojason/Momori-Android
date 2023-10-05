137306d24df83a311395e0a4a199cecc14033b9d
bswlkh0310 <hhhello0507@gmail.com>
2023-10-05 11:58:22 +0900

    library install & gradle to kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,88 @@
+plugins {
+    id ("com.android.application")
+    id ("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.nohjason.momori"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.nohjason.momori"
+        minSdk = 28
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles (
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.2"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+
+dependencies {
+    // retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+
+    // google map
+    implementation ("com.google.maps.android:maps-compose:1.0.0")
+    implementation ("com.google.android.gms:play-services-maps:18.0.2")
+
+    // compose
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation ("androidx.activity:activity-compose:1.5.1")
+    implementation ("androidx.compose.ui:ui")
+    implementation ("androidx.compose.ui:ui-graphics")
+    implementation ("androidx.compose.ui:ui-tooling-preview")
+    implementation ("androidx.compose.material3:material3")
+    androidTestImplementation (platform("androidx.compose:compose-bom:2022.10.00"))
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4")
+    debugImplementation ("androidx.compose.ui:ui-tooling")
+    debugImplementation ("androidx.compose.ui:ui-test-manifest")
+    implementation (platform("androidx.compose:compose-bom:2022.10.00"))
+
+    // kotlin
+    implementation ("androidx.core:core-ktx:1.8.0")
+    implementation (platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
+
+    // android
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+
+    // test
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+}
---
+++ app/src/main/java/com/nohjason/momori/component/appbar/MomoriTopBar.kt
@@ -0,0 +1,15 @@
+package com.nohjason.momori.component.appbar
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MomoriTopBar(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Column {
+        
+    }
+}
